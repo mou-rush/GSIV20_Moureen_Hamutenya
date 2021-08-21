9810ba3f20ce66e5b7a9ce25616c5416e691e14d
Moureen <moureenham@gmail.com>
2021-08-21 16:54:30 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
-// import Navbar from './components/Navbar';
-// import Planets from './components/Planets';
 import Movies from "./components/Movies";
+import Navbar from "./components/Navbar";
 import { ReactQueryDevtools } from "react-query-devtools";
 import { QueryClient, QueryClientProvider } from "react-query";
 function App() {
-  //let [page, setPage] = useState("planets");
   const queryClient = new QueryClient();
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <div className="App">
-          <h1>Star Wars Info</h1>
-          {/* <Navbar setPage={setPage} /> */}
           <div className="content">
+            <Navbar />
             <Movies />
           </div>
         </div>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { useQuery } from "react-query";
 import Movie from "./Movie";
+import "./Movies.css";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
 
 const fetchMovies = async () => {
   const res = await fetch(
@@ -14,23 +21,42 @@ const Movies = () => {
 
   return (
     <>
-      <section className="grid">
-        {data.results.map((movie) => {
-          const { id, vote_average, overview, title } = movie;
+      {status === "loading" && <div>Loading data</div>}
 
-          return (
-            <article key={id}>
-              {/* <Link to={`/resources/${id}`} style={{ textDecoration: "none" }}> */}
-              <div className="details">
-                <h2>{title}</h2>
-                <h1>{overview}</h1>
-                <h1>{vote_average}</h1>
-              </div>
-              {/* </Link> */}
-            </article>
-          );
-        })}
-      </section>
+      {status === "error" && <div>Error fetching data</div>}
+
+      {status === "success" && (
+        <section className="grid">
+          {data.results.map((movie) => {
+            const {
+              id,
+              vote_average,
+              overview,
+              title,
+              poster_path: image,
+            } = movie;
+
+            return (
+              <Card key={id}>
+                {/* <Link to={`/resources/${id}`} style={{ textDecoration: "none" }}> */}
+                <CardMedia
+                  image={image}
+                  title={title}
+
+                  // <h2>{title}</h2>
+                  // <p>{overview}</p>
+                  // <h1>{vote_average}</h1>
+                />
+                <CardContent>
+                  <h2>{title}</h2>
+                  <p>{overview}</p>
+                </CardContent>
+                {/* </Link> */}
+              </Card>
+            );
+          })}
+        </section>
+      )}
     </>
   );
 };
